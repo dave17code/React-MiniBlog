306f1d2abbd46de3d5e9e2730ffb39eab3f9dfe6
dave17code <dave17code@gmail.com>
2024-09-19 10:17:10 +0900

    MiniBlog

--- src/component/page/PostViewPage.jsx
+++ src/component/page/PostViewPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import styled from "styled-components";
 import CommentList from "../list/CommentList";
@@ -7,15 +7,91 @@ import Button from "../ui/Button";
 import data from "../../data.json";
 
 const Wrapper = styled.div`
-    padding: 16px;
-    width: calc(100% - 32px);
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+  padding: 16px;
+  width: calc(100% - 32px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Container = styled.div`
-    width: 100%;
-    max-width: 720px;
-`
+  width: 100%;
+  max-width: 720px;
+`;
+
+const PostContainer = styled.div`
+  padding: 8px 16px;
+  border: 1px solid gray;
+  border-radius: 8px;
+`;
+
+const TitleText = styled.p`
+  font-size: 28px;
+  font-weight: 500;
+`;
+
+const ContentText = styled.p`
+  font-size: 20px;
+  line-height: 32px;
+  white-space: pre-wrap;
+`;
+
+const CommentLabel = styled.p`
+  font-size: 16px;
+  font-weight: 500;
+`;
+
+function PostViewPage() {
+  const navigate = useNavigate();
+  const { postId } = useParams();
+
+  // postId에 해당하는 데이터를 찾아서 post에 저장
+  const post = data.find((item) => item.id == postId);
+
+  const [comment, setComment] = useState("");
+
+  // post가 없으면 "존재하지 않는 포스트"를 보여줌
+  if (!post) {
+    return (
+      <Wrapper>
+        <Container>
+          <p>존재하지 않는 포스트입니다.</p>
+          <Button
+            title="뒤로 가기"
+            onClick={() => navigate("/")}
+          />
+        </Container>
+      </Wrapper>
+    );
+  }
+
+  return (
+    <Wrapper>
+      <Container>
+        <Button
+          title="뒤로 가기"
+          onClick={() => navigate("/")}
+        />
+        <PostContainer>
+          <TitleText>{post.title}</TitleText>
+          <ContentText>{post.content}</ContentText>
+        </PostContainer>
+
+        <CommentLabel>댓글</CommentLabel>
+        <CommentList comments={post.comments || []} />
+        <TextInput
+          height={40}
+          value={comment}
+          onChange={(event) => setComment(event.target.value)}
+        />
+        <Button
+          title="댓글 작성하기"
+          onClick={() => navigate("/")}
+        />
+      </Container>
+    </Wrapper>
+  );
+}
+
+export default PostViewPage;
--- src/component/page/PostWritePage.jsx
+++ src/component/page/PostWritePage.jsx
@@ -5,57 +5,54 @@ import TextInput from "../ui/TextInput";
 import Button from "../ui/Button";
 
 const Wrapper = styled.div`
-    padding: 16px;
-    width: calc(100% - 32px);
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+  padding: 16px;
+  width: calc(100% - 32px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Container = styled.div`
-    width: 100%;
-    max-width: 720px;
-
-    & > * {
-        :not(:last-child) {
-            margin-bottom: 16px;
-        }
+  width: 100%;
+  max-width: 720px;
+  & {
+    :not(:last-child) {
+      margin-bottom: 16px;
     }
+  }
 `;
 
-function PostWritePage(props) {
-    const navigate = useNavigate();
-    const [title, setTitle] = useState("");
-    const [content, setContent] = useState("");
+function PostWritePage() {
+  const navigate = useNavigate();
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
-    return (
-        <Wrapper>
-            <Container>
-                <TextInput 
-                    height={20}
-                    value={title}
-                    onChange={(event) => {
-                        setTitle(event.target.value);
-                    }}
-                />
-                <TextInput 
-                    height={480}
-                    value={content}
-                    onChange={(event) => {
-                        setContent(event.target.value);
-                    }}
-                />
-                <Button 
-                    title="글 작성하기"
-                    onClick={() => {
-                        navigate("/");
-                    }}
-                />
-            </Container>
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <Container>
+        <TextInput 
+          height={20}
+          value={title}
+          placeholder="제목을 입력하세요"
+          onChange={(event) => setTitle(event.target.value)}
+        />
+        <TextInput 
+          height={480}
+          value={content}
+          placeholder="내용을 입력하세요"
+          onChange={(event) => setContent(event.target.value)}
+        />
+        <Button
+          title="글 작성하기"
+          onClick={() => {
+            // 저장 후 메인 페이지로 이동 (추후 저장 기능 구현 필요)
+            navigate("/");
+          }}
+        />
+      </Container>
+    </Wrapper>
+  );
 }
 
 export default PostWritePage;
-
